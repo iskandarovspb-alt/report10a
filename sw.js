2738345f388d160a7ee7b863b17d67e1bc3d20f0
iskandarovspb-alt <iskandarov.spb@gmail.com>
2026-09-03 18:38:33 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,14 +1,1 @@
-const CACHE_NAME = 'energetikov-v2';
-const ASSETS = ['index.html', 'manifest.json', 'icon.png'];
 
-self.addEventListener('install', (e) => {
-  e.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)));
-});
-
-// Исправленный обработчик: пропускает POST-запросы формы напрямую в Google
-self.addEventListener('fetch', (e) => {
-  if (e.request.method === 'POST') {
-    return; // Просто выходим и не мешаем форме отправляться
-  }
-  e.respondWith(caches.match(e.request).then((res) => res || fetch(e.request)));
-});
